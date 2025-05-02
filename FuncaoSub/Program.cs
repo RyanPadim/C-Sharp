56fd6cd8f7eec7abd22a926424c0388178a2d34f
Ryan Padim Ricardo <Ryan.p.ricardo@gmail.com>
2025-05-02 17:44:33 -0300

    Add files via upload

---
+++ FuncaoSub/Program.cs
@@ -0,0 +1,9 @@
+﻿static int subtrair(int a, int b)
+{
+    int subtrair = a - b;
+    return subtrair;
+}
+
+int resultado = subtrair(5, 8);
+Console.WriteLine("O resultado da subtração foi: " + resultado);
+Console.ReadKey();
