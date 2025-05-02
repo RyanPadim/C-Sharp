bc65a06ee07308a30d95393e20649cd18bc8add8
Ryan Padim Ricardo <Ryan.p.ricardo@gmail.com>
2025-05-02 17:43:51 -0300

    Add files via upload

---
+++ Aula_08_soma/Program.cs
@@ -0,0 +1,10 @@
+﻿int n1;
+int n2;
+int soma;
+Console.WriteLine("Digite o primeiro número");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+n2 = int.Parse(Console.ReadLine());
+soma = n1 + n2;
+Console.WriteLine(soma);
+Console.ReadKey();
