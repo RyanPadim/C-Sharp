efcd995a0973f6b405e1f8771dd35a6a413a99d0
Ryan Padim Ricardo <Ryan.p.ricardo@gmail.com>
2025-05-09 17:30:42 -0300

    Add files via upload

---
+++ Herança/Program.cs
@@ -0,0 +1,15 @@
+﻿using Herança;
+
+aluno usuario = new aluno();
+usuario.setNome("João Paulo");
+usuario.setIdade(30);
+usuario.setSexo("Masculino");
+usuario.setCurso("Engenharia civil");
+usuario.setRegistro(35);
+
+Console.WriteLine("Nome: " + usuario.getNome());
+Console.WriteLine("idade: " + usuario.getIdade());
+Console.WriteLine("Sexo: " + usuario.getSexo());
+Console.WriteLine("Cursando o curso: " + usuario.getCurso());
+Console.WriteLine("Número de registro acadêmico: " + usuario.getRegistro());
+Console.ReadKey();
