04be891a3d615f36a4d33f4d02d350f0abbdf284
Ryan Padim Ricardo <Ryan.p.ricardo@gmail.com>
2025-05-16 17:39:46 -0300

    Add files via upload

---
+++ Aula16/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula16
+{
+    internal static class Calculadora
+    {
+        private static double pi = 3.14;
+
+        public static int adicao(int numero1, int numero2)
+        {
+            return (numero1 + numero2);
+        }
+
+        public static int subtracao(int numero1, int numero2)
+        {
+            return (numero1 - numero2);
+        }
+
+        public static int multiplicacao(int numero1, int numero2)
+        {
+            return (numero1 * numero2);
+        }
+
+        public static int divisao(int numero1, int numero2)
+        {
+            return (numero1 / numero2);
+        }
+
+        public static double areaCirculo(int raio)
+        {
+            double area = 0;
+            area = Calculadora.pi * raio * raio;
+            return area;
+        }
+    }
+}
---
+++ Aula16/Program.cs
@@ -0,0 +1,13 @@
+﻿using Aula16;
+
+Console.WriteLine("Soma = " + Calculadora.adicao(3, 8).ToString());
+
+Console.WriteLine("Subtração = " + Calculadora.subtracao(5, 2).ToString());
+
+Console.WriteLine("Multiplicação = " + Calculadora.multiplicacao(9, 5).ToString());
+
+Console.WriteLine("Divisão = " + Calculadora.divisao(10, 5).ToString());
+
+Console.WriteLine("Área do círculo = " + Calculadora.areaCirculo(6).ToString());
+
+Console.ReadKey();
