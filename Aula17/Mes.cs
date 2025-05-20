513a3d5e5aa1e5b8e6157b9b0d5d80ec202842a4
Ryan Padim Ricardo <Ryan.p.ricardo@gmail.com>
2025-05-20 19:59:05 -0300

    Add files via upload

---
+++ Aula17/Mes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula17
+{
+    internal enum Mes
+    {
+        Janeiro = 1,
+        Fevereiro = 2,
+        Março = 3,
+        Abril = 4,
+        Maio = 5,
+        Junho = 6,
+        Julho = 7,
+        Agosto = 8,
+        Setembro = 9,
+        Outubro = 10,
+        Novembro = 11,
+        Dezembro = 12
+    }
+    class Program
+    {
+        static void main()
+        {
+            Mes mes1 = Mes.Janeiro;
+            int i1 = (int)Mes.Janeiro;
+
+            System.Console.WriteLine(mes1);
+            System.Console.WriteLine(i1);
+            Console.ReadKey();
+
+            Mes mes2 = Mes.Julho;
+            int i2 = (int)Mes.Julho;
+
+            System.Console.WriteLine(mes2);
+            System.Console.WriteLine(i2);
+            Console.ReadKey();
+        }
+    }
+}
